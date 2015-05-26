4f7b0ad4dde1bec630f84e52675dfdd4849cea54
antass <anna.krentowicz@gmail.com>
2015-05-25 20:21:53 -0400

    update site

--- publish.sh
+++ publish.sh
@@ -9,7 +9,7 @@ git push
 ## Copy site to repository for github hosting
 echo 'Uploading site to Dreamhost'
 #rsync -avz --checksum --delete /home/ania/Dropbox/labnotebook/_site/ antass@quicksilver.dreamhost.com:aniatassinari.com/labnotebook  
-rsync -avz --checksum --delete --chmod=Fa-w /Users/antass/Dropbox/mama-shared/mkrentowicz.com/site/ antass@holly.dreamhost.com:mkrentowicz.com
+rsync -avz --checksum --delete --chmod=Fa-w /Users/antass/Dropbox/mama-shared/mkrentowicz.com/site/HTML/ antass@holly.dreamhost.com:mkrentowicz.com
 echo "Site updated and uploaded to Dreamhost"
 
 

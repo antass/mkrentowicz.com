fa567033769095401d1f7c30331f2d0518676f74
antass <anna.krentowicz@gmail.com>
2015-05-25 20:12:57 -0400

    update site

---
+++ publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Commit changes to get correct timestamp
+echo 'Committing changes to git' 
+git add .
+git commit -am "update site"
+git push
+
+## Copy site to repository for github hosting
+echo 'Uploading site to Dreamhost'
+#rsync -avz --checksum --delete /home/ania/Dropbox/labnotebook/_site/ antass@quicksilver.dreamhost.com:aniatassinari.com/labnotebook  
+rsync -avz --checksum --delete --chmod=Fa-w /Users/antass/Dropbox/mama_shared/mkrentowicz.com/site/ antass@holly.dreamhost.com:mkrentowicz.com
+echo "Site updated and uploaded to Dreamhost"
+
+
